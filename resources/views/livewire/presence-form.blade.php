70811dd200e36f845f17c80f0696c23b35c81912
andrewdef1 <andrewbelanda@gmail.com>
2023-02-01 18:52:52 +0900

    add date range for absence

--- resources/views/livewire/presence-form.blade.php
+++ resources/views/livewire/presence-form.blade.php
@@ -8,7 +8,7 @@
 
 
         {{-- jika absen pulang sudah dimulai, dan karyawan belum absen masuk dan belum absen pulang --}}
-        @if ($attendance->data->is_end && !$data['is_has_enter_today'])
+        @if ($attendance->data->is_end && !$data['is_has_enter_today'] && $attendance->data->in_date_range)
         <button class="btn btn-primary px-3 py-2 btn-sm fw-bold d-block w-100" wire:click="sendLateEnterPresence"
             wire:loading.attr="disabled" wire:target="sendLateEnterPresence">Masuk (Hanya yang lupa absen masuk!!!)</button>
         @endif
@@ -16,50 +16,50 @@
 
 
     {{-- jika tidak menggunakan qrcode (button) dan karyawan saat ini tidak menekan tombol izin --}}
-    @if (!$attendance->data->is_using_qrcode && !$data['is_there_permission'])
+    @if (!$attendance->data->is_using_qrcode && !$data['is_there_permission'] && $attendance->data->in_date_range)
 
     {{-- jika belum absen dan absen masuk sudah dimulai --}}
-    @if ($attendance->data->is_start && !$data['is_has_enter_today'])
+    @if ($attendance->data->is_start && !$data['is_has_enter_today'] && $attendance->data->in_date_range)
     <button class="btn btn-primary px-3 py-2 btn-sm fw-bold d-block w-100 mb-2" wire:click="sendEnterPresence"
         wire:loading.attr="disabled" wire:target="sendEnterPresence">Masuk</button>
     <a href="{{ route('home.permission', $attendance->id) }}"
         class="btn btn-info px-3 py-2 btn-sm fw-bold d-block w-100">Izin</a>
     @endif
 
-    @if ($data['is_has_enter_today'])
+    @if ($data['is_has_enter_today'] && $attendance->data->in_date_range)
     <div class="alert alert-success">
         <small class="d-block fw-bold text-success">Anda sudah berhasil mengirim absensi masuk.</small>
     </div>
     @endif
 
     {{-- jika absen pulang sudah dimulai, dan karyawan sudah absen masuk dan belum absen pulang --}}
-    @if ($attendance->data->is_end && $data['is_has_enter_today'] && $data['is_not_out_yet'])
+    @if ($attendance->data->is_end && $data['is_has_enter_today'] && $data['is_not_out_yet'] && $attendance->data->in_date_range)
     <button class="btn btn-primary px-3 py-2 btn-sm fw-bold d-block w-100" wire:click="sendOutPresence"
         wire:loading.attr="disabled" wire:target="sendOutPresence">Pulang</button>
     @endif
 
     {{-- sudah absen masuk dan absen pulang --}}
-    @if ($data['is_has_enter_today'] && !$data['is_not_out_yet'])
+    @if ($data['is_has_enter_today'] && !$data['is_not_out_yet'] && $attendance->data->in_date_range)
     <div class="alert alert-success">
         <small class="d-block fw-bold text-success">Anda sudah melakukan absen masuk dan absen pulang.</small>
     </div>
     @endif
 
     {{-- jika sudah absen masuk dan belum saatnya absen pulang --}}
-    @if ($data['is_has_enter_today'] && !$attendance->data->is_end)
+    @if ($data['is_has_enter_today'] && !$attendance->data->is_end && $attendance->data->in_date_range)
     <div class="alert alert-danger">
         <small class="fw-bold">Belum saatnya melakukan absensi pulang.</small>
     </div>
     @endif
     @endif
 
-    @if($data['is_there_permission'] && !$data['is_permission_accepted'])
+    @if($data['is_there_permission'] && !$data['is_permission_accepted'] && $attendance->data->in_date_range)
     <div class="alert alert-info">
         <small class="fw-bold">Permintaan izin sedang diproses (atau masih belum di terima).</small>
     </div>
     @endif
 
-    @if($data['is_there_permission'] && $data['is_permission_accepted'])
+    @if($data['is_there_permission'] && $data['is_permission_accepted'] && $attendance->data->in_date_range)
     <div class="alert alert-success">
         <small class="fw-bold">Permintaan izin sudah diterima.</small>
     </div>
--- resources/views/partials/attendance-badges.blade.php
+++ resources/views/partials/attendance-badges.blade.php
@@ -1,10 +1,10 @@
-@if($attendance->data->is_holiday_today)
+@if($attendance->data->is_holiday_today && $attendance->data->in_date_range)
 <span class="badge text-bg-success rounded-pill">Hari Libur</span>
 @else
 
-@if ($attendance->data->is_start)
+@if ($attendance->data->is_start && $attendance->data->in_date_range)
 <span class="badge bg-primary rounded-pill">Jam Masuk</span>
-@elseif($attendance->data->is_end)
+@elseif($attendance->data->is_end && $attendance->data->in_date_range)
 <span class="badge text-bg-warning rounded-pill">Jam Pulang</span>
 @else
 <span class="badge text-bg-danger rounded-pill">Tutup</span>
